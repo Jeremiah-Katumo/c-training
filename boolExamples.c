15b8ef20cfdd7e2197959286f444b714df53e42d
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-03-26 13:56:22 +0300

    booleans

--- boolExamples.c
+++ boolExamples.c
@@ -5,47 +5,37 @@ int main() {
     int votingAge = 18;
 
     if (myAge >= votingAge) {
-    printf("Old enough to vote!");
+        printf("Old enough to vote!\n");
     } else {
-    printf("Not old enough to vote.");
+        printf("Not old enough to vote.\n");
     }
 
     ////////////////////
     int doorCode = 1337;
 
     if (doorCode == 1337) {
-      printf("Correct code.\nThe door is now open.");
+      printf("Correct code.\nThe door is now open.\n");
     } else {
-      printf("Wrong code.\nThe door remains closed.");
+      printf("Wrong code.\nThe door remains closed.\n");
     }
     ///////////////////
     int myNum = 10; // Is this a positive or negative number?
 
     if (myNum > 0) {
-      printf("The value is a positive number.");
+      printf("The value is a positive number.\n");
     } else if (myNum < 0) {
-      printf("The value is a negative number.");
+      printf("The value is a negative number.\n");
     } else {
       printf("The value is 0.");
     }
 
     ///////////////////
-    int myAge = 25;
-    int votingAge = 18;
+    int myVal = 5;
 
-    if (myAge >= votingAge) {
-      printf("Old enough to vote!");
+    if (myVal % 2 == 0) {
+      printf("%d is even.\n", myVal);
     } else {
-      printf("Not old enough to vote.");
-    }
-
-    ///////////////////
-    int myNum = 5;
-
-    if (myNum % 2 == 0) {
-      printf("%d is even.\n", myNum);
-    } else {
-      printf("%d is odd.\n", myNum);
+      printf("%d is odd.\n", myVal);
     }
 
     return 0;
